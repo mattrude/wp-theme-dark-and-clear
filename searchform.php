c57e25459d0351904d57f064b7253ab84aa6e07a
Matt Rude <github@mattrude.com>
2009-07-03 00:25:47 -0400

    corrected formating

--- searchform.php
+++ searchform.php
@@ -1,5 +1,6 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-<div><input type="text" name="s" id="s" onblur="searchblur(this);" onfocus="searchfocus(this);" value="Search Pages" />
-<input type="hidden" id="searchsubmit" value="Search" />
-</div>
+ <div>
+  <input type="text" name="s" id="s" onblur="searchblur(this);" onfocus="searchfocus(this);" value="Search Pages" />
+  <input type="hidden" id="searchsubmit" value="Search" />
+ </div>
 </form>
