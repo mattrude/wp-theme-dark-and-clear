a20838e01e53fd05dbccf879961dcc3dc947120b
Matt Rude <github@mattrude.com>
2009-07-05 18:49:08 -0400

    Google Analytics now will only display for users not logged in.

--- footer.php
+++ footer.php
@@ -6,26 +6,31 @@
 </div>
 </div>
 
-<!--This is another plugin hook-->
-<?php wp_footer(); ?>
+<!--footer plugin hook-->
+<?php wp_footer();
 
-<?php 
-global $Panel;
-$GAEnabled = $Panel->Settings('GoogleAnalyticsEnabled');
-$GAID = $Panel->Settings('GoogleAnalyticsID');
-if ($GAEnabled == 'true') {
-	if ($GAID != NULL) { ?>
-		<!--Google Analytics-->
-		<script type="text/javascript">
-		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-		</script>
-		<script type="text/javascript">
-		try {
-		var pageTracker = _gat._getTracker("<?php echo $GAID; ?>");
-		pageTracker._trackPageview();
-		} catch(err) {}</script>
-	<?php }
+if ( is_user_logged_in() ) { 
+	?><!--User is logged in, so this request will Not be tracked by Google Analytics-->
+<?php
+} else {
+	global $Panel;
+	$GAEnabled = $Panel->Settings('GoogleAnalyticsEnabled');
+	$GAID = $Panel->Settings('GoogleAnalyticsID');
+	if ($GAEnabled == 'true') {
+		if ($GAID != NULL) { ?>
+			<!--Google Analytics tracking script-->
+			<script type="text/javascript">
+			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+			</script>
+			<script type="text/javascript">
+			try {
+			var pageTracker = _gat._getTracker("<?php echo $GAID; ?>");
+			pageTracker._trackPageview();
+			} catch(err) {}</script>
+<?php 		}
+	}
+
 } ?>
 <!--Closeing tags-->
 </body>
