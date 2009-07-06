021db32fecad688d50a6e578ed444ce01cb2d1a9
Matt Rude <github@mattrude.com>
2009-07-05 22:48:52 -0400

    removed dead IE6 lines

--- header_plain.php
+++ header_plain.php
@@ -8,12 +8,6 @@
 <title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<!--[if IE 6]>
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/ie6.css" />
-<![endif]-->
-<!--[if IE]>
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/ie.css" />
-<![endif]-->
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
