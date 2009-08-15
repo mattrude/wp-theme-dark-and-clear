0582a036337dfc2093dc3ce08c4b805d11b5c4da
Matt Rude <github@mattrude.com>
2009-08-15 15:25:57 -0500

    Moved favicon from the root of the site to the images directory of the theme, this should be configurable.

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 
 <head>
-<link rel='shortcut icon' href='http://www.mattrude.com/favicon.ico' />
+<link rel='shortcut icon' href='<?php bloginfo('template_url') ?>/images/favicon.ico' />
 <?php wp_head(); ?>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
