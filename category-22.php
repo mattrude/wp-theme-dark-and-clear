3c699956b6f939ca05cb8815a0a10cafa2edf1f1
Matt Rude <github@mattrude.com>
2009-08-15 20:31:30 -0500

    Added page links to tweet gallery

--- category-22.php
+++ category-22.php
@@ -9,27 +9,23 @@
 	</div>
 		<!--This is "The Loop"-->
 		<?php while (have_posts()) : the_post(); ?>
-			<div <?php post_class(); ?> id=tweet_post>
+			<div <?php post_class(); ?>>
+			<div id=tweet_post>
 				<img src="<?php bloginfo('template_url'); ?>/images/twitter-logo.jpg" width="50" height="50" align='left' style='margin-right: 5px;' />
 				<?php the_excerpt(); ?>
-				<div id=tweet_date>
+			</div>
+			<div id=tweet_date>
 				<?php global $wp_query; ?>
                         	<?php $tweet_id = get_post_meta( $wp_query->post->ID, 'aktt_twitter_id', true ); ?>
 				Posted to <a href="http://twitter.com">Twitter</a> by <?php the_author() ?> on <a href="http://twitter.com/mdrude/status/<?php echo $tweet_id ?>"><?php the_time('F jS, h:ma T Y ') ?></a>
-				</div>	
-				<!--	
-				<p class="postmetadata">Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Responses &#187;', '1 Response &#187;', '% Responses &#187;'); ?> <?php the_tags('<br />Tags: ', ', ', '<br />'); ?></p>
-				-->
-			
+			</div>	
 			</div><!--close post class and post# id-->
 		<?php endwhile; ?>
-		
-		<!--
+		<br />	
 		<div class="navigation">
-			<div class="txtalignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-			<div class="txtalignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+			<div class="txtalignright"><?php next_posts_link('Older Entries &raquo;') ?></div>
+			<div class="txtalignleft"><?php previous_posts_link('&laquo; Newer Entries') ?></div>
 		</div>
-		-->
 	<?php else : ?>
 
 		<h2 class="center">Oops! Couldn't find what you were looking for. Maybe you'll want to search for it:</h2>
