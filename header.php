fc53cdd0e81307444e76010d919799bf46bb2715
Matt Rude <github@mattrude.com>
2009-08-21 20:31:43 -0400

    removed old rss setup

--- header.php
+++ header.php
@@ -45,18 +45,6 @@ if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 		&nbsp; <!--Mozilla hack to show background-->
 		<div id="header">
 			
-			<!--UNCOMMENT THE FOLLOWING SECTION OF CODE FOR RSS AND RSS VIA EMAIL
-				SUBSCRIPTION BUTTONS
-			<div id="subscribe">
-				<div id="feedbubble"><div id="feedbubble_inner1"><div id="feedbubble_inner2">Click here to subscribe to the RSS Feed in your favorite feed reader</div><div id="feedbubble_pointer">&nbsp;</div></div></div>
-				<div id="emailbubble"><div id="emailbubble_inner1"><div id="emailbubble_pointer">&nbsp;</div><div id="spacer">&nbsp;</div><div id="emailbubble_inner2">Click here to have new posts sent straight to your email</div></div></div>
-				
-				<a class="img" href="<?php bloginfo('rss2_url'); ?>" title=""><img src="<?php bloginfo('template_url'); ?>/images/rss_icon.gif" alt="" onmouseover="showRSS();" onmouseout="hideRSS();" /></a>
-				<br />
-				<a class="img" href="http://www.feedburner.com/fb/a/emailverifySubmit?feedId=1516518&loc=en_US" title=""><img src="<?php bloginfo('template_url'); ?>/images/email_icon.gif" alt="" onmouseover="showEmail();" onmouseout="hideEmail();" /></a>
-			</div>
-			-->
-			
 			<div id="masthead_wrapper">
 				<h1 id="masthead"><a class="standard" href="<?php bloginfo('url'); ?>">mattrude.com</a></h1><h2 id="tagline"><?php bloginfo('description'); ?></h2>
 			</div>
