20769c28802642651982c609d77585f4dd342622
Matt Rude <github@mattrude.com>
2009-08-21 23:01:07 -0400

    more gallery updates

--- category-23.php
+++ category-23.php
@@ -1,22 +1,38 @@
-<?php get_header(); ?>
+<?php
 
+get_header();
+
+$cat_title = '<a href="'.get_category_link(intval(get_query_var('cat'))).'">'.single_cat_title('', false).'</a>'; ?>
+
+<h1>Matt Rude's Personal Photo Gallery</h1>
 <div id="content">
 
-<div id="contentleft">
-
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-<div id="gallerypost"><a href="<?php the_permalink() ?>" rel="bookmark"><img src="<?php echo get_post_meta($post->ID, "gallery thumbnail", true); ?>" alt="<?php echo get_post_meta($post->ID, "gallery thumbnail", true); ?> Gallery" height='100' border='0' align='left' style='margin-left: 5px; margin-right: 5px;' /></a>
-<h1 style="margin:0px 0px 2px 10px;"><?php the_title(); ?></h1><?php the_excerpt(); ?>
-</div>
+	<div id="gallerypost-<?php the_ID(); ?>" class="gallerypost">
+		<div id="gallerypost_thumbnail-<?php the_ID(); ?>">
+			<?php global $wp_query; ?>
+			<?php $gallery_thumbnail = get_post_meta( $wp_query->post->ID, 'gallery thumbnail', true ); ?>
+			<a href="<?php the_permalink() ?>" rel="bookmark"><img src="<?php echo $gallery_thumbnail; ?>" alt="<?php echo get_post_meta( $post->ID, 'gallery thumbnail', true); ?> Gallery" border='1' align='left' style='margin-right: 5px;' /></a>
+		</div>
+		<div id="gallerypost_body-<?php the_ID(); ?>" class="gallerypost_body">
+			<h2><a rel="bookmark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<div class="entry">
+				<?php the_excerpt(); ?>
+			</div>
+		</div>
+		<div id="gallerypost_sub-<?php the_ID(); ?>">
+			<?php echo get_the_term_list( $post->ID, 'people', 'People in this Gallery: ', ', ', '' ); ?>
+		</div>
+	</div>
 <?php endwhile; else: ?>
 
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 
 <?php endif; ?>
-
 </div>
+<?php 
+get_sidebar();
 
-<?php include(TEMPLATEPATH."/sidebar.php");?>
+get_footer();
 
-<?php get_footer(); ?>
+?>
